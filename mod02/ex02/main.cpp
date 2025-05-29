5e72c9f68a2c592560ab8f18b475b0c2aafc329d
Lilly Kunne <sikunne@c2r5s1.42wolfsburg.de>
2025-05-29 16:56:01 +0200

    Redid Makefiles, now they have better naming and readability

--- mod02/ex02/main.cpp
+++ mod02/ex02/main.cpp
@@ -14,14 +14,34 @@
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+	{
+		Fixed a;
+		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max( a, b ) << std::endl;
+	}
+	{
+		Fixed a(5);
+		Fixed b(a);
+		Fixed null;
+		Fixed c(6.9696969f);
+		Fixed exactA(0.00390625f);
+		Fixed exactB(0.06640625f);
+		
+		b++;
+		std::cout << a << " + " << b << " = " << a+b << std::endl;
+		std::cout << a << " - " << b << " = " << a-b << std::endl;
+		std::cout << a << " / " << b << " = " << a/b << std::endl;
+		std::cout << a << " / " << null << " = " << a/null << std::endl;
+		std::cout << a << " / " << c << " = " << a/c << std::endl;
+		std::cout << a << " * " << c << " = " << a*c << std::endl;
+		std::cout << exactA << " + " << exactB << " = " << exactA + exactB << std::endl;
+		std::cout << "... should be " << 0.00390625f + 0.06640625f << std::endl;
+	}
 	return 0;
 }
